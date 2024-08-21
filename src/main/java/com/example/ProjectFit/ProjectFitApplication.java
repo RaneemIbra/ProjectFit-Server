adc138f217842abf8863241c38422498983d0496
RaneemIbra <132738285+RaneemIbra@users.noreply.github.com>
2024-08-21 21:03:29 +0300

    first push

---
+++ src/main/java/com/example/ProjectFit/ProjectFitApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjectFit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectFitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProjectFit/ProjectFitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjectFit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
