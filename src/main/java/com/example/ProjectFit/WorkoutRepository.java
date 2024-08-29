b71420700461b24a50d50d00c26552cb61a08147
RaneemIbra <132738285+RaneemIbra@users.noreply.github.com>
2024-08-29 14:44:47 +0300

    made changes to fix the workout filtered lists

--- src/main/java/com/example/ProjectFit/WorkoutRepository.java
+++ src/main/java/com/example/ProjectFit/WorkoutRepository.java
@@ -2,8 +2,10 @@ package com.example.ProjectFit;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WorkoutRepository extends JpaRepository<Workout,Long> {
-    Workout findWorkoutByType(String type);
-    Workout findWorkoutByDuration(int duration);
-    Workout findWorkoutByMuscle(String muscle);
+    List<Workout> findWorkoutByType(String type);
+    List<Workout> findWorkoutByDuration(int duration);
+    List<Workout> findWorkoutByMuscle(List<String> muscle);
 }
